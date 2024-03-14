ce124f65fa4b02baa962b70979c021a34f363aa7
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2024-03-14 20:20:40 +0600

    configured tailwind for some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#5630FF",
+				secondary: "#ECECEC",
+				"deep-blue": "#00156A",
+				"light-blue": "#F3F6F9",
+				"pale-blue": "#B9C1D9",
+				"royal-indigo": "#2B3674",
+				"tranquil-blue": "#8897AD",
+				"silver-cloud": "#F3F3F3",
+				"grayish-blue": "#F3F6F9",
+				"han-purple": "#5630FF",
+				"vivid-blue": "#4318FF",
+				textDark: "#464E5F",
+				"dark-indigo": "#0C1421",
+				"dark-slate-blue": "#313957",
+				"vibrant-green": "#20DC49",
+			},
+		},
 	},
 	plugins: [],
 };
