09f6f1919a009162325cc256bead1b5bfa3ce5bc
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2024-03-14 20:16:51 +0600

    created sorting in dashboard based on food category

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,17 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Navbar from "../components/NavBar";
 import Banner from "../components/Banner";
 import FoodCard from "../components/FoodCard";
 import { FoodItems } from "../utils/food-items";
 
 const HomePage: FC = () => {
+	const [selectedCategory, setSelectedCategory] = useState("All");
+
+	const filteredItems =
+		selectedCategory === "All"
+			? FoodItems
+			: FoodItems.filter((item) => item.category === selectedCategory);
+
 	return (
 		<div>
 			<div>
@@ -12,19 +19,34 @@ const HomePage: FC = () => {
 				<Banner />
 				<div className="my-10">
 					<h1 className="text-4xl font-bold text-center mb-5">Products List</h1>
-					<div className="flex justify-center">
-						{FoodItems.map((item, index) => (
+					<select
+						className="ml-8 px-5 py-3 rounded-sm outline-none"
+						onChange={(e) => setSelectedCategory(e.target.value)}
+					>
+						<option value="All">All Items</option>
+						<option value="Pasta">Pasta</option>
+						<option value="Salad">Salad</option>
+						<option value="Burger">Burger</option>
+						{/* Add more options based on your categories */}
+					</select>
+					<div className="flex justify-center flex-wrap">
+						{filteredItems.map((item, index) => (
 							<FoodCard
 								key={index}
 								title={item.title}
 								price={item.price}
 								image={item.image}
-								catergory={item.catergory}
+								category={item.category}
 								rating={item.rating}
+								customStyle="w-[30%]"
 							/>
 						))}
 					</div>
 				</div>
+			</div>
+			{/* Footer */}
+			<div className="flex justify-center py-5 bg-gray-200">
+				<p>All rights reserved by ICE-19 Lab-4.</p>
 			</div>
 		</div>
 	);
