9f31767b394ebe1c9277ab1fede346a7a9c081eb
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2024-03-14 20:14:59 +0600

    Update FoodCard and dummy data

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -3,26 +3,38 @@ import { FC } from "react";
 interface FoodCardProps {
 	image: string;
 	title: string;
-	catergory: string;
+	category: string;
 	price: number;
 	rating: number;
 	restaurant_name?: string;
+	customStyle?: string;
 }
 
 const FoodCard: FC<FoodCardProps> = (props: FoodCardProps) => {
-	const { title, image, catergory, price, rating } = props;
+	const { title, image, category, price, rating, customStyle } = props;
 
 	return (
-		<div className="ring-1 p-5 rounded-lg">
-			<div className="h-[180px] p-5 ">
-				<img src={image} alt="image" className="h-[100%]" />
+		<div className={`ring-1 p-5 rounded-lg m-5 ${customStyle}`}>
+			<div className="w-[100%] h-[300px] flex justify-center">
+				<img
+					src={image}
+					alt="image"
+					className="w-[100%] h-[100%] hover:w-[99%] rounded-lg"
+				/>
 			</div>
 			<div className="flex justify-center flex-col pt-4 rounded-md text-[#5B5B5B]">
 				<h3 className="text-3xl ">{title}</h3>
-				<p className="text-base">{catergory}</p>
-				<p className="text-3xl ">{rating}</p>
-				<p className="text-gray-700">{price.toFixed(2)} tk</p>
+				<p className="text-base">{category}</p>
+				<div className="flex justify-between">
+					<p className="text-gray-700">{price.toFixed(2)} tk</p>
+					<p className="text-sm ">
+						<span className="">☆</span> {rating}
+					</p>
+				</div>
 			</div>
+			<button className="bg-gray-700 text-white px-5 py-2 rounded-md">
+				Add to Cart
+			</button>
 		</div>
 	);
 };
