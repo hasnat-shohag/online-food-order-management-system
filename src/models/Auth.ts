fd6d263e0c7e9e279b1388eedd40b0be3e1ebb07
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2024-03-17 19:44:18 +0600

    created useSession hook

--- src/models/Auth.ts
+++ src/models/Auth.ts
@@ -1,6 +1,17 @@
+import { SESSION_STATUS } from "../constants/Global";
+export interface ISession {
+	session: { email: string } | null;
+	status: SESSION_STATUS;
+}
+
 export interface ISignupRequest {
 	name: string;
 	email: string;
 	password: string;
 	confirmPassword: string;
+}
+
+export interface ILoginRequest {
+	email: string;
+	password: string;
 }
