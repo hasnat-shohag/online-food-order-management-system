0e7a0a5d1d528449b2e409ff86b8cea9de8c5fac
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2024-03-17 13:10:55 +0600

    added basic routes

---
+++ src/constants/Route.ts
@@ -0,0 +1,12 @@
+export const routes = {
+	signup: {
+		title: "Signup",
+		key: "signup",
+		path: "/auth/signup",
+	},
+  login: {
+    title: 'Login',
+    key: 'login',
+    path: '/auth/login',
+  },
+};
